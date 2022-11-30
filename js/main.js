74d1748cb88c210f45fa82d0c362bfdd0a7d84b3
brian-jc-yu <brian.jc.yu@gmail.com>
2022-11-30 07:05:42 -0800

    공통 common.css common.js 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-const searchEl = document.querySelector(`.search`); //document는 HTML 자체를 의미함
-const searchInputEl = searchEl.querySelector(`input`);
 
-searchEl.addEventListener(`click`, function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener(`focus`, function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색'); /*Attribute는 HTML의 속성*/
-});
-
-searchInputEl.addEventListener('blur', function () {
-  /* blur 포커스 해제된 상태 */
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 /* 스크롤하면 뱃지가 사라지도록 하는 기능 */
 const badgeEl = document.querySelector('header .badges');
@@ -165,5 +149,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //연도만 뽑아냄
